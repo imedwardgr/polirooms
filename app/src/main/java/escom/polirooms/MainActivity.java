9ca43e97bc35c8ed78bda6a3354cfcde68200be9
imedward <gomezreduardo@hotmail.com>
2019-11-22 12:41:41 -0600

    comentarios

--- app/src/main/java/escom/polirooms/MainActivity.java
+++ app/src/main/java/escom/polirooms/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import escom.polirooms.R;
 
 public class MainActivity extends AppCompatActivity {
-    //comentario de prueba
+    //comentario de paolafea
     //respuesta al comentario xd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
