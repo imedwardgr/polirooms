f03e7d1741f304f9a3eb0b17378d360925466321
imedward <gomezreduardo@hotmail.com>
2019-11-25 00:04:35 -0600

    choose photo menu

---
+++ app/src/main/java/escom/polirooms/choose_image.java
@@ -0,0 +1,99 @@
+package escom.polirooms;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+
+public class choose_image extends DialogFragment {
+    LinearLayout galleryButton;
+    LinearLayout cameraButton;
+    String image;
+
+    public static final int PICK_IMAGE = 1;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.fragment_choose_image, null);
+        builder.setView(v);
+
+        galleryButton = v.findViewById(R.id.choose_from_gallery_button);
+        cameraButton = v.findViewById(R.id.choose_from_camera_button);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                }
+                else
+                {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+
+
+        return builder.create();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == PICK_IMAGE) {
+            image = data.getDataString();
+        }
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
+            image = data.getDataString();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(getContext(), "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(getContext(), "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
--- app/src/main/java/escom/polirooms/reg_husped_form.java
+++ app/src/main/java/escom/polirooms/reg_husped_form.java
@@ -1,14 +1,39 @@
 package escom.polirooms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class reg_husped_form extends AppCompatActivity {
+    DialogFragment chooseImageDialog;
+    Button ineButton;
+    Button ipnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_husped_form);
+
+        chooseImageDialog = new choose_image();
+
+        ineButton = findViewById(R.id.form_huesped_ine);
+        ipnButton = findViewById(R.id.form_huesped_ipn);
+
+        ineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImageDialog.show(getSupportFragmentManager(),"chooseImage");
+            }
+        });
+
+        ipnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImageDialog.show(getSupportFragmentManager(),"chooseImage");
+            }
+        });
     }
 }
