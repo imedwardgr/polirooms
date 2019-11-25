047e4f9d8c1244809cb10092dec8d9ab87feea69
imedward <gomezreduardo@hotmail.com>
2019-11-24 19:07:24 -0600

    registro layout tipo de usuario

--- app/src/main/java/escom/polirooms/reg_select_type_of_user.java
+++ app/src/main/java/escom/polirooms/reg_select_type_of_user.java
@@ -16,17 +16,7 @@ public class reg_select_type_of_user extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_select_type_of_user);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
