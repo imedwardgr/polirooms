11c96281258800a5939427626b0318a620d028d0
imedward <gomezreduardo@hotmail.com>
2019-11-24 21:52:35 -0600

    registro estudiante

---
+++ app/src/main/java/escom/polirooms/terms_and_conditions.java
@@ -0,0 +1,14 @@
+package escom.polirooms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class terms_and_conditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+    }
+}
