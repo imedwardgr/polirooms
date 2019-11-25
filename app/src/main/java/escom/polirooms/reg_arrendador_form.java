2bdbbf98cad336e6a80bb3e86e8feeb8fb36213f
imedward <gomezreduardo@hotmail.com>
2019-11-24 19:19:32 -0600

    registro layout tipo de usuario

---
+++ app/src/main/java/escom/polirooms/reg_arrendador_form.java
@@ -0,0 +1,14 @@
+package escom.polirooms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reg_arrendador_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_arrendador_form);
+    }
+}
--- app/src/main/java/escom/polirooms/reg_select_type_of_user.java
+++ app/src/main/java/escom/polirooms/reg_select_type_of_user.java
@@ -1,5 +1,6 @@
 package escom.polirooms;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class reg_select_type_of_user extends AppCompatActivity {
+    Button huespedButton;
+    Button arrendadorButton;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_select_type_of_user);
+
+        huespedButton = findViewById(R.id.register_huesped_button);
+        arrendadorButton = findViewById(R.id.register_arrendador_button);
+
+        huespedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(reg_select_type_of_user.this, reg_husped_form.class);
+                startActivity(i);
+            }
+        });
+
+        arrendadorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(reg_select_type_of_user.this, reg_arrendador_form.class);
+                startActivity(i);
+            }
+        });
 
     }
 
